008497d507f56196609cd72e8e41be824689a1fe
Ben Mezger <me@benmezger.nl>
2017-04-06 15:50:57 -0300

    Manage the application

---
+++ web/manage.py
@@ -0,0 +1,33 @@
+import os
+
+from flask import Flask
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View
+
+from views.views import ServiceView
+from views.views import IDPView
+from views.views import SPView
+from views.views import LDAPView
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+Bootstrap(app)
+nav = Nav()
+
+@nav.navigation()
+def global_nav():
+    return Navbar("Gidlab", View("Configuracao de servico", "serviceview"))
+
+nav.init_app(app)
+
+app.add_url_rule('/', view_func=ServiceView.as_view('serviceview', template_name="service.html"))
+app.add_url_rule('/idp', view_func=IDPView.as_view("idpview", template_name="idp.html"))
+app.add_url_rule('/sp', view_func=SPView.as_view("spview", template_name="sp.html"))
+app.add_url_rule('/ldap', view_func=SPView.as_view("ldapview", template_name="ldap.html"))
+
+manager = Manager(app)
+
+if __name__ == "__main__":
+    manager.run()
