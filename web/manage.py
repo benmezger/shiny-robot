a955026d736312e58920d7eaa509a80065a1b753
Ben Mezger <me@benmezger.nl>
2017-04-06 23:01:28 -0300

    Imported ProecessView

--- web/manage.py
+++ web/manage.py
@@ -10,6 +10,7 @@ from views.views import ServiceView
 from views.views import IDPView
 from views.views import SPView
 from views.views import LDAPView
+from views.views import ProcessView
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
@@ -26,6 +27,7 @@ app.add_url_rule('/', view_func=ServiceView.as_view('serviceview', template_name
 app.add_url_rule('/idp', view_func=IDPView.as_view("idpview", template_name="idp.html"))
 app.add_url_rule('/sp', view_func=SPView.as_view("spview", template_name="sp.html"))
 app.add_url_rule('/ldap', view_func=SPView.as_view("ldapview", template_name="ldap.html"))
+app.add_url_rule('/process', view_func=ProcessView.as_view("processview", template_name="process.html"))
 
 manager = Manager(app)
 
