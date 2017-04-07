3ca6271c1eac74cd8cae735ad428adda0830e316
Ben Mezger <me@benmezger.nl>
2017-04-07 16:05:55 -0300

    Country information form and extra fields of LDAP Fixed url_rule for LDAP

--- web/manage.py
+++ web/manage.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*
 import os
 
 from flask import Flask
@@ -20,14 +21,14 @@ nav = Nav()
 
 @nav.navigation()
 def global_nav():
-    return Navbar("Gidlab", View("Configuração de Serviços", "serviceview"))
+    return Navbar("Gidlab", View(u"Configuração de Serviços", "serviceview"))
 
 nav.init_app(app)
 
 app.add_url_rule('/', view_func=ServiceView.as_view('serviceview', template_name="service.html"))
 app.add_url_rule('/idp', view_func=IDPView.as_view("idpview", template_name="idp.html"))
 app.add_url_rule('/sp', view_func=SPView.as_view("spview", template_name="sp.html"))
-app.add_url_rule('/ldap', view_func=SPView.as_view("ldapview", template_name="ldap.html"))
+app.add_url_rule('/ldap', view_func=LDAPView.as_view("ldapview", template_name="ldap.html"))
 app.add_url_rule('/process', view_func=ProcessView.as_view("processview", template_name="process.html"))
 app.add_url_rule("/ssh", view_func=SSHView.as_view("sshview", template_name="ssh.html"))
 
--- web/views/forms.py
+++ web/views/forms.py
@@ -10,6 +10,7 @@ from wtforms import BooleanField
 from wtforms import IntegerField
 from wtforms import validators
 from wtforms import PasswordField
+from wtforms.fields.html5 import EmailField
 
 class ServiceForm(Form): # TODO: Fix validation, make sure at least one if required
     ldap = BooleanField("LDAP")
@@ -24,29 +25,83 @@ class NetworkForm(Form):
             validators=[validators.Required()]) # TODO, split
     hostname = StringField(u"Hostname", default=socket.gethostname(),
             validators=[validators.Required()])
-    gateway = StringField("Gateway", validators=[validators.Required()]) # TODO
-    dns = StringField("DNS", validators=[validators.Required()]) # TODO
-    mask = StringField("Mascara", validators=[validators.Required()])
+    gateway = StringField(u"Gateway", validators=[validators.Required()]) # TODO
+    dns = StringField(u"DNS", validators=[validators.Required()]) # TODO
+    mask = StringField(u"Mascara", validators=[validators.Required()])
 
-class IDPForm(NetworkForm):
-    port = SelectMultipleField("Porta", validators=[validators.Required()],
+br_states = [ # this is temporary
+        ('AC', u'Acre'),
+        ('AL', u'Alagoas'),
+        ('AP', u'Amapá'),
+        ('AM', u'Amazonas'),
+        ('BA', u'Bahia'),
+        ('CE', u'Ceará'),
+        ('DF', u'Distrito Federal'),
+        ('ES', u'Espírito Santo'),
+        ('GO', u'Goiás'),
+        ('MA', u'Maranhão'),
+        ('MT', u'Mato Grosso'),
+        ('MS', u'Mato Grosso do Sul'),
+        ('MG', u'Minas Gerais'),
+        ('PA', u'Pará'),
+        ('PB', u'Paraíba'),
+        ('PR', u'Paraná'),
+        ('PE', u'Pernambuco'),
+        ('PI', u'Piauí'),
+        ('RJ', u'Rio de Janeiro'),
+        ('RN', u'Rio Grande do Norte'),
+        ('RS', u'Rio Grande do Sul'),
+        ('RO', u'Rondônia'),
+        ('RR', u'Roraima'),
+        ('SC', u'Santa Catarina'),
+        ('SP', u'São Paulo'),
+        ('SE', u'Sergipe'),
+        ('TO', u'Tocantins')]
+
+class CountryForm(Form):
+    city = StringField(u"Cidade", validators=[validators.Required()])
+    state = SelectField(u"Estado", choices=br_states, validators=[
+        validators.Required()])
+    organization_name = StringField(u"Nome da organização", validators=[
+        validators.Required()])
+    technical_name = StringField(u"Nome do técnico", validators=[validators.Required()])
+    technical_email = EmailField(u"Email do técnico", validators=[validators.Required(),
+        validators.Email()])
+    country = SelectField(u"País", choices=[("BR", "Brasil")], validators=[validators.Required()])
+    organization_initials = technical_name = StringField(u"Iniciais da organização",
+            validators=[validators.Required()])
+
+class IDPForm(NetworkForm, CountryForm):
+    port = SelectMultipleField(u"Porta", validators=[validators.Required()],
             choices=[(443, 443), (80, 80)], coerce=int)
     institution_name = StringField(u"Nome da instituição", validators=[
         validators.Required()])
-    keystore_pwd = PasswordField("Senha para o keystore", validators=[
-        validators.DataRequired(), validators.EqualTo("confirm",
-            message="As senhas devem coincidir.")])
-    confirm = PasswordField("Confirme sua senha.", message="Esse campo é obrigatorio.")
+    keystore_pwd = PasswordField(u"Senha para o keystore", validators=[
+        validators.DataRequired(), validators.EqualTo("confirm", message=u"As senhas devem coincidir.")])
+    confirm = PasswordField(u"Confirme sua senha.")
+    # technical_email = EmailField(u"Email do técnico", validators=[validators.Required(),
+    #     validators.Email()])
+    # technical_name = StringField(u"Nome do técnico", validators=[validators.Required()])
+    install_path = StringField(u"Caminho da instalação", validators=[validators.Required()])
 
 class SPForm(Form):
     pass
 
-class LDAPForm(NetworkForm):
-    url = StringField("URL LDAP", validators=[validators.Required(),
+class LDAPForm(NetworkForm, CountryForm):
+    url = StringField(u"URL LDAP", validators=[validators.Required(),
         validators.URL()])
-    query_dn = StringField("DN de consulta", validators=[validators.Required()])
-    read_dn = StringField("DN de leitura", validators=[validators.Required()])
-    port = IntegerField("Porta", default=389, validators=[validators.Required()])
+    query_dn = StringField(u"DN de consulta", validators=[validators.Required()])
+    read_dn = StringField(u"DN de leitura", validators=[validators.Required()])
+    port = IntegerField(u"Porta", default=389, validators=[validators.Required()])
+    researcher_password = PasswordField(u"Senha para o keystore", validators=[
+        validators.DataRequired(), validators.EqualTo("confirm",
+            message=u"As senhas devem coincidir.")])
+    confirm = PasswordField(u"Confirme sua senha.")
+    starttls = BooleanField(u"Usar StartTLS", validators=[validators.Required()])
+    ldap_password = PasswordField(u"Senha para o keystore", validators=[
+        validators.DataRequired(), validators.EqualTo("confirm_ldap",
+            message=u"As senhas devem coincidir.")])
+    confirm_ldap = PasswordField(u"Confirme sua senha.")
 
 class SSLForm(Form):
     pass
