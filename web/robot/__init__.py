5ccb61ddd9a5e8e06d9a8296e4d1a9ffa66ccdf1
Ben Mezger <me@benmezger.nl>
2017-04-18 09:30:16 -0300

    Keep view configuration sepparetly for each app

---
+++ web/robot/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*
+import os
+
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View
+
+from views import ServiceView
+from views import IDPView
+from views import SPView
+from views import LDAPView
+from views import ProcessView
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+Bootstrap(app)
+nav = Nav()
+
+@nav.navigation()
+def global_nav():
+    return Navbar("Gidlab", View(u"Configuração de Serviços", "serviceview"))
+
+nav.init_app(app)
+
+app.add_url_rule('/', view_func=ServiceView.as_view('serviceview', template_name="service.html"))
+app.add_url_rule('/idp', view_func=IDPView.as_view("idpview", template_name="idp.html"))
+app.add_url_rule('/sp', view_func=SPView.as_view("spview", template_name="sp.html"))
+app.add_url_rule('/ldap', view_func=LDAPView.as_view("ldapview", template_name="ldap.html"))
+app.add_url_rule('/process', view_func=ProcessView.as_view("processview", template_name="process.html"))
+# app.add_url_rule("/ssh", view_func=SSHView.as_view("sshview", template_name="ssh.html"))
+
+
